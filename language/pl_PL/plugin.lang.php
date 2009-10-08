c2dec437996f903e51f6233aa06cea5a6b39d0e3
voyteckst <voyteckst@piwigo.org>
2009-10-08 19:33:01 +0000

    git-svn-id: http://piwigo.org/svn/extensions/lightbox@3991 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+//Polish translation by voyteckst
+$lang['lb_display'] = 'Wyœwietlanie';
+$lang['lb_display_name'] = 'Wyœwietlaj tytu³ obrazu';
+$lang['lb_name_link'] = 'Link tytu³owy';
+$lang['lb_none'] = '¯aden';
+$lang['lb_picture'] = 'Strona obrazu';
+$lang['lb_high'] = 'Obraz wysokiej jakoœci';
+
+$lang['lb_display_arrows'] = 'Pokazuj strza³ki nawigacyjne';
+$lang['lb_all_category'] = 'U¿yj wszystkich orazów w kategori';
+$lang['lb_yes'] = 'Tak';
+$lang['lb_no'] = 'Nie';
+
+$lang['lb_theme_transition'] = 'Styl & przejœcia';
+$lang['lb_transition'] = 'Przejœcia';
+$lang['lb_theme'] = 'Styl';
+$lang['lb_transition_elastic'] = 'Elastyczne';
+$lang['lb_transition_fade'] = 'Fade';
+$lang['lb_transition_none'] = 'Brak';
+$lang['lb_transition_speed'] = 'Prêdkoœæ przejœæ';
+
+$lang['lb_dimensions'] = 'wymiary';
+$lang['lb_initial_width'] = 'Pocz¹tkowa szerokoœæ';
+$lang['lb_initial_height'] = 'Pocz¹tkowa wysokoœæ';
+
+$lang['lb_fixed_width'] = 'Sta³a szerokoœæ';
+$lang['lb_fixed_height'] = 'Sta³a wysokoœæ';
+
+$lang['lb_configuration_saved'] = 'Konfiguracja zosta³a zapisana';
+$lang['lb_default_parameters'] = 'Konfiguracja domyœlna';
+$lang['lb_default_parameters_saved'] = 'Domyœlna konfiguracja zosta³a zapisana';
+
+?>
