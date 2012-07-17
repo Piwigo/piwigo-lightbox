c4b02449dc419bc16027c46b45c1f877bbe32a91
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-17 14:10:54 +0000

    [lightbox] Update da_DK, thanks to : thomas1956
    git-svn-id: http://piwigo.org/svn/extensions/lightbox@16805 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['lb_all_category'] = 'Anvend alle billeder i kategorien';
+$lang['lb_configuration_saved'] = 'Indstillinger gemt';
+$lang['lb_default_parameters_saved'] = 'Standard indstillinger gemt';
+$lang['lb_default_parameters'] = 'Standard indstillinger';
+$lang['lb_dimensions'] = 'Størrelse';
+$lang['lb_display_arrows'] = 'Vis navigations pile';
+$lang['lb_display_name'] = 'Vis titel på billede';
+$lang['lb_display'] = 'Visning';
+$lang['lb_fixed_height'] = 'Fast højde';
+$lang['lb_fixed_width'] = 'Fast bredde';
+$lang['lb_high'] = 'HD billede';
+$lang['lb_initial_height'] = 'Start højde';
+$lang['lb_initial_width'] = 'Start bredde';
+$lang['lb_name_link'] = 'Link i titel';
+$lang['lb_no'] = 'Nej';
+$lang['lb_none'] = 'Ingen';
+$lang['lb_picture'] = 'Billed side';
+$lang['lb_theme_transition'] = 'Tema og effekt';
+$lang['lb_theme'] = 'Tema';
+$lang['lb_transition_elastic'] = 'Elastisk';
+$lang['lb_transition_fade'] = 'Opløs';
+$lang['lb_transition_none'] = 'Ingen';
+$lang['lb_transition_speed'] = 'Hastighed for effekt';
+$lang['lb_transition'] = 'Effekt';
+$lang['lb_yes'] = 'Ja';
+?>
