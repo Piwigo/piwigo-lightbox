e2e7ddc451688d9ae9dfcf0beba508254234bdf6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-04-18 12:00:41 +0000

    Add ja_JP, thanks to Trumpet
    git-svn-id: http://piwigo.org/svn/extensions/lightbox@31932 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['lb_transition_elastic'] = '弾性';
+$lang['lb_transition_fade'] = 'フェード';
+$lang['lb_transition_none'] = 'なし';
+$lang['lb_transition_speed'] = '遷移速度';
+$lang['lb_yes'] = 'はい';
+$lang['lb_all_category'] = 'カテゴリ内のすべての画像を使う';
+$lang['lb_configuration_saved'] = '設定が保存されました';
+$lang['lb_default_parameters'] = '既定の設定';
+$lang['lb_default_parameters_saved'] = '既定の設定が保存されました';
+$lang['lb_dimensions'] = '寸法';
+$lang['lb_display'] = '表示する';
+$lang['lb_display_arrows'] = '案内の矢印を表示する';
+$lang['lb_display_name'] = '画像の題名を表示する';
+$lang['lb_fixed_height'] = '固定高さ';
+$lang['lb_fixed_width'] = '固定幅';
+$lang['lb_high'] = '高解像画像';
+$lang['lb_initial_height'] = '初期高さ';
+$lang['lb_initial_width'] = '初期幅';
+$lang['lb_name_link'] = '題目リンク';
+$lang['lb_no'] = 'いいえ';
+$lang['lb_none'] = 'なし';
+$lang['lb_picture'] = '画像ページ';
+$lang['lb_theme'] = 'テーマ';
+$lang['lb_theme_transition'] = 'テーマと遷移';
+$lang['lb_transition'] = '遷移';
