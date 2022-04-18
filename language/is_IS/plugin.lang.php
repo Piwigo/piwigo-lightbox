78eca2ac8d829474656deee4d9f0729352f05d71
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-18 21:01:40 +0000

    Update is_IS, thanks to gudjonsi
    git-svn-id: http://piwigo.org/svn/extensions/lightbox@32759 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['lb_yes'] = 'Já';
+$lang['lb_dimensions'] = 'Mál';
+$lang['lb_display'] = 'Birta';
+$lang['lb_display_arrows'] = 'Sýna leiðsöguörvar';
+$lang['lb_display_name'] = 'Birta titil myndar';
+$lang['lb_fixed_height'] = 'Föst hæð';
+$lang['lb_fixed_width'] = 'Föst breidd';
+$lang['lb_high'] = 'Háskerpu mynd';
+$lang['lb_initial_height'] = 'Upphafshæð';
+$lang['lb_initial_width'] = 'Upphafsbreidd';
+$lang['lb_name_link'] = 'Titilhlekkur';
+$lang['lb_no'] = 'Nei';
+$lang['lb_none'] = 'Enginn';
+$lang['lb_picture'] = 'Myndasíða';
+$lang['lb_theme'] = 'Þema';
+$lang['lb_theme_transition'] = 'Þema og umskipti';
+$lang['lb_transition'] = 'Umskipti';
+$lang['lb_transition_elastic'] = 'Teygjanlegt';
+$lang['lb_transition_fade'] = 'Fölna';
+$lang['lb_transition_none'] = 'Enginn';
+$lang['lb_transition_speed'] = 'Umskiptahraði';
+$lang['lb_all_category'] = 'Nota allar myndir í flokki';
+$lang['lb_configuration_saved'] = 'Stilling vistuð';
+$lang['lb_default_parameters'] = 'Sjálfgefin stilling';
+$lang['lb_default_parameters_saved'] = 'Sjálfgefin stilling vistuð';
