6b57add5aabbd01f7ce96ea9cf322c694f99c86d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 06:30:16 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['lb_transition_elastic'] = 'גמיש';
+$lang['lb_transition_fade'] = 'להתעלם';
+$lang['lb_transition_none'] = 'אין';
+$lang['lb_transition_speed'] = 'מהירות המעבר';
+$lang['lb_yes'] = 'כן';
+$lang['lb_all_category'] = 'השתמש בכל התמונות בקטגוריה';
+$lang['lb_configuration_saved'] = 'הגדרות נשמרו';
+$lang['lb_default_parameters'] = 'הגדרת ברירת מחדל';
+$lang['lb_default_parameters_saved'] = 'הגדרת ברירת מחדל נשמרה';
+$lang['lb_dimensions'] = 'ממדים';
+$lang['lb_display'] = 'תצוגה';
+$lang['lb_display_arrows'] = 'הצג חצים לניווט';
+$lang['lb_display_name'] = 'הצג כותרת תמונה';
+$lang['lb_fixed_height'] = 'גובה קבוע';
+$lang['lb_fixed_width'] = 'רוחב קבוע';
+$lang['lb_high'] = 'תמונת רזולוציה גבוהה';
+$lang['lb_initial_height'] = 'גובה ראשוני';
+$lang['lb_initial_width'] = 'רוחב ראשוני';
+$lang['lb_name_link'] = 'קישור כותרת';
+$lang['lb_no'] = 'לא';
+$lang['lb_none'] = 'אין';
+$lang['lb_picture'] = 'עמוד תמונה';
+$lang['lb_theme'] = 'ערכת עיצוב';
+$lang['lb_theme_transition'] = 'ערכת עיצוב ומעבר';
+$lang['lb_transition'] = 'מעבר';
