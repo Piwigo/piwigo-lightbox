a1bf1cf998bebf211fbe2fe27409773dc17fbd0d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-04-14 20:30:27 +0200

    Update fi_FI, thanks to Jusaus

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['lb_transition_elastic'] = 'Joustava';
+$lang['lb_transition_fade'] = 'Häivytys';
+$lang['lb_transition_none'] = 'Ei yhtään';
+$lang['lb_transition_speed'] = 'Siirtymän nopeus';
+$lang['lb_yes'] = 'Kyllä';
+$lang['lb_all_category'] = 'Käytä kategorian kaikkia kuvia';
+$lang['lb_configuration_saved'] = 'Asetukset tallennettu';
+$lang['lb_default_parameters'] = 'Oletusasetukset';
+$lang['lb_default_parameters_saved'] = 'Oletusasetukset tallennettu';
+$lang['lb_dimensions'] = 'Mitat';
+$lang['lb_display'] = 'Näyttö';
+$lang['lb_display_arrows'] = 'Näytä navigaationuolet';
+$lang['lb_display_name'] = 'Näytä kuvan otsikko';
+$lang['lb_fixed_height'] = 'Kiinteä korkeus';
+$lang['lb_fixed_width'] = 'Kiinteä leveys';
+$lang['lb_high'] = 'Korkean resoluution kuva';
+$lang['lb_initial_height'] = 'Alkukorkeus';
+$lang['lb_initial_width'] = 'Alkuleveys';
+$lang['lb_name_link'] = 'Otsikkolinkki';
+$lang['lb_no'] = 'Ei';
+$lang['lb_none'] = 'Ei mitään';
+$lang['lb_picture'] = 'Kuvasivu';
+$lang['lb_theme'] = 'Teema';
+$lang['lb_theme_transition'] = 'Teema & siirtymä';
+$lang['lb_transition'] = 'Siirtymä';
