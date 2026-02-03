a61917c415137371fa521e31b398b1cba3b95f41
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-03 06:57:13 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['lb_transition_elastic'] = '엘라스틱';
+$lang['lb_transition_fade'] = '페이드';
+$lang['lb_transition_none'] = '없음';
+$lang['lb_transition_speed'] = '전환 속도';
+$lang['lb_yes'] = '예';
+$lang['lb_all_category'] = '카테고리의 모든 이미지 사용';
+$lang['lb_configuration_saved'] = '설정 저장됨';
+$lang['lb_default_parameters'] = '기본 설정';
+$lang['lb_default_parameters_saved'] = '기본 설정 저장됨';
+$lang['lb_dimensions'] = '크기';
+$lang['lb_display'] = '표시';
+$lang['lb_display_arrows'] = '탐색 화살표 표시';
+$lang['lb_display_name'] = '이미지 제목 표시';
+$lang['lb_fixed_height'] = '고정 높이';
+$lang['lb_fixed_width'] = '고정 너비';
+$lang['lb_high'] = '고화질 이미지';
+$lang['lb_initial_height'] = '초기 높이';
+$lang['lb_initial_width'] = '초기 너비';
+$lang['lb_name_link'] = '제목 링크';
+$lang['lb_no'] = '아니요';
+$lang['lb_none'] = '없음';
+$lang['lb_picture'] = '이미지 페이지';
+$lang['lb_theme'] = '테마';
+$lang['lb_theme_transition'] = '테마 및 전환';
+$lang['lb_transition'] = '전환';
